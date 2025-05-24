9b84f1efe78b577bdcb9177d0cee39b1f0a254be
upishanker <upishanker1@gmail.com>
2025-05-24 15:41:44 -0500

    finised user controller

--- src/main/java/com/upishanker/gradehub/controller/UserController.java
+++ src/main/java/com/upishanker/gradehub/controller/UserController.java
@@ -4,37 +4,35 @@ import com.upishanker.gradehub.model.Course;
 import com.upishanker.gradehub.model.User;
 import com.upishanker.gradehub.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-@Autowired
 public class UserController {
     private UserService userService;
     public UserController(UserService userService) {
         this.userService = userService;
     }
+    @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
+    @GetMapping("/{userId}")
     public User getUser(@PathVariable long userId) {
         return userService.getUserById(userId);
     }
+    @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
-    public User updateUser(@PathVariable long userId, @RequestBody User updatedUser) {
-        return userService.updateUser(userId, updatedUser);
+    @PatchMapping("/{userId}/username")
+    public User changeUsername(@PathVariable long userId, @RequestBody String newUsername) {
+        return userService.changeUsername(userId, newUsername);
     }
+    @DeleteMapping("/{userId}")
     public void  deleteUser(@PathVariable long userId) {
         userService.deleteUser(userId);
-    }
-    public List<Course> getUserCourses(@PathVariable long userId) {
-        return userService.getUserCourses(userId);
     }
 }
